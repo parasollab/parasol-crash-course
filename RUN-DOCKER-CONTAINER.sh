58c871b10c164dac74557e9797a6768a7c70e57d
Courtney McBeth <clm325@cornell.edu>
2024-05-29 12:47:20 -0500

    trying to get display working

--- RUN-DOCKER-CONTAINER.sh
+++ RUN-DOCKER-CONTAINER.sh
@@ -17,6 +17,7 @@ docker compose -p ${PROJECT} -f ./docker/docker-compose.yml up -d
 ################################################################################
 
 # Display GUI through X Server by granting full access to any external client.
+HOST_IP=$(ipconfig getifaddr en0)
 xhost +
 # xhost +local:root
 
@@ -27,14 +28,14 @@ case "$3" in
   ( "" )
   case "$2" in
     ( "" )
-    docker exec -i -t ${CONTAINER} bash
+    docker exec -e DISPLAY=${HOST_IP}:0 -i -t ${CONTAINER} bash
     ;;
     ( * )
-    docker exec -i -t ${CONTAINER} bash -i -c "~/ppl_cc_ws/docker/ppl-crash-course/scripts/run-command-repeatedly.sh $2"
+    docker exec -e DISPLAY=${HOST_IP}:0 -i -t ${CONTAINER} bash -i -c "~/ppl_cc_ws/docker/ppl-crash-course/scripts/run-command-repeatedly.sh $2"
   esac
   ;;
   ( *".launch")
-  docker exec -i -t ${CONTAINER} bash -i -c "~/ppl_cc_ws/docker/ppl-crash-course/scripts/run-ros2-launch-repeatedly.sh $2 $3"
+  docker exec -e DISPLAY=${HOST_IP}:0 -i -t ${CONTAINER} bash -i -c "~/ppl_cc_ws/docker/ppl-crash-course/scripts/run-ros2-launch-repeatedly.sh $2 $3"
   ;;
   ( * )
   echo "Failed to enter the Docker container '${CONTAINER}': '$3' is not a valid argument value (needs to be a launch file or empty)."
